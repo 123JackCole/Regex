59556cd98f88fa34506c9fc57f5b0755285193b7
123JackCole <123jack.cole@gmail.com>
2020-09-19 18:00:43 -0700

    added in test cases

--- regex.js
+++ regex.js
@@ -1,4 +1,11 @@
-let regex1 = new RegExp('x+yz');
+const regex1 = new RegExp('love');
+const regex2 = /regex/i;
 
-let regex2 = /x+zy/;
+const string = "i love regex and regex loves me";
+
+string.match(regex1); // ["love"]
+string.match(regex2); // ["regex"]
+string.replace(/i/, 'I'); // "I love regex and regex loves me"
+
+/me/.test(string); // true
 
