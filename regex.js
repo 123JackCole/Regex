0a6f60d3bfcd7f9c34a28df5e3d1b9e04afc6e5e
123JackCole <123jack.cole@gmail.com>
2020-09-19 18:17:39 -0700

    brackets

--- regex.js
+++ regex.js
@@ -1,11 +1,17 @@
 const regex1 = new RegExp('love');
 const regex2 = /regex/i;
 
-const string = "i love regex and regex loves me";
+let string = "i love regex and regex loves me";
 
 string.match(regex1); // ["love"]
 string.match(regex2); // ["regex"]
 string.replace(/i/, 'I'); // "I love regex and regex loves me"
 
 /me/.test(string); // true
+
+string = 'test';
+
+string.match(/test/); // ["test"]
+string.match(/[test]/); // ["t"]
+string.match(/[a-f]/); // ["e"]
 
